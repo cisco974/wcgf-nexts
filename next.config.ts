f11b6641cd93f4aa8a08693de0ee8510506bd31b
cisco974 <vitryfrancois@gmail.com>
2025-02-24 11:51:28 +0400

    test deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   distDir: "build",
+  output: "export",
   experimental: {
     // This is experimental but can
     // be enabled to allow parallel threads
