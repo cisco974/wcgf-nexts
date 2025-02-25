eaef7b052977b25fd880f8643c5559e001d359e9
cisco974 <vitryfrancois@gmail.com>
2025-02-25 09:50:16 +0400

    test deploy

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
-echo -e "#!/bin/bash\nnpm run start" > start.sh
+#!/bin/bash
+# start-server.sh
+
+# 1️⃣ Activation de l'environnement Node.js
+source /home/cayi7350/nodevenv/test.wcgf.com/22/bin/activate
+
+# 2️⃣ Accès au répertoire de l'application
+cd /home/cayi7350/test.wcgf.com
+
+# 3️⃣ Démarrage du serveur Next.js
+npm run start
