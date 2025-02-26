8d8955b70cc4d71e9e7faa8d2f24b4724b614d9b
cisco974 <vitryfrancois@gmail.com>
2025-02-26 13:19:32 +0400

    last test deploy

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
 const { exec } = require("child_process");
 
-// Exécute directement `next start`
-console.log("🔄 Démarrage de Next.js...");
-
-exec("node_modules/.bin/next start", { shell: true }, (err, stdout, stderr) => {
-  if (err) {
-    console.error("❌ Erreur lors du démarrage :", err);
-    return;
-  }
-  console.log(`✅ Next.js démarré avec succès !\n${stdout}`);
-  if (stderr) console.error(`⚠️ Stderr :\n${stderr}`);
-});
-
-// Indique à Phusion Passenger que le script est prêt
-module.exports = {};
+exec("npm run start", { shell: true });
