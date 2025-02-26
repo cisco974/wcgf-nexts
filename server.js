4c76f53c5b576a7e100958a6122fbd2f3112d432
cisco974 <vitryfrancois@gmail.com>
2025-02-26 11:34:11 +0400

    test deploy

--- server.js
+++ server.js
@@ -1,21 +1,4 @@
-const npm = require("npm");
+let shell = require("shelljs");
 
-npm.load({}, function (err) {
-  if (err) {
-    console.error("❌ Erreur lors du chargement de npm :", err);
-    return;
-  }
-
-  console.log("🔄 Démarrage de l’application avec npm run start...");
-
-  npm.commands.run(["start"], function (err, data) {
-    if (err) {
-      console.error("❌ Erreur lors de l’exécution de npm run start :", err);
-    } else {
-      console.log("🚀 Next.js est lancé avec succès !");
-    }
-  });
-});
-
-// Indique à Phusion Passenger que le script est prêt
-module.exports = {};
+shell.exec("echo shell.exec works");
+shell.exec("npm run start");
