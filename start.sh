ed2ba9089d9a5c36b9e3354ae1b5c0d40a2d2f56
cisco974 <vitryfrancois@gmail.com>
2025-03-03 09:50:23 +0400

    update deploy

--- start.sh
+++ start.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 LOG_FILE="/home/cayi7350/test.wcgf.com/nextjs.log"
 
+echo "[$(date)] Vérification du processus Next.js..." | tee -a "$LOG_FILE"
+
+# Vérifier si un processus "npm run start" est déjà actif
+EXISTING_PID=$(pgrep -f "npm run start")
+
+if [ -n "$EXISTING_PID" ]; then
+  echo "[$(date)] L'application est déjà en cours d'exécution (PID: $EXISTING_PID). Démarrage annulé." | tee -a "$LOG_FILE"
+  exit 1
+fi
+
 echo "[$(date)] Démarrage de l'application Next.js..." | tee -a "$LOG_FILE"
 
 cd /home/cayi7350/test.wcgf.com || exit 1
