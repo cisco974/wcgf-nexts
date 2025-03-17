75381125205add5f5108cfeed0eeb111dec689bb
cisco974 <vitryfrancois@gmail.com>
2025-03-17 09:18:58 +0400

    mise a jour des dir

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: "build",
   trailingSlash: true,
   sassOptions: {
     additionalData: `@import "./src/app/styles/_variables.scss";`,
